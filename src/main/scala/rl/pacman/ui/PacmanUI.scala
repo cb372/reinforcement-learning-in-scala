4bfd17fc54e9238b984706effdb1d0e78c9a3196
Chris Birchall <chris.birchall@gmail.com>
2018-11-20 13:26:48 +0000

    Give the ghosts some eyes!

--- src/main/scala/rl/pacman/ui/PacmanUI.scala
+++ src/main/scala/rl/pacman/ui/PacmanUI.scala
@@ -78,15 +78,55 @@ object PacmanUI {
 
   private def drawGame(canvas: html.Canvas, state: GameState, actionTaken: Move): Unit = {
     val ctx         = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-    val pixelWidth  = 50
-    val pixelHeight = 50
+    val pixelSize   = 50
+    val pixelCentre = pixelSize / 2
 
-    def drawGhost(ghost: Location, colour: String): Unit = {
+    def drawEye(ghost: Location, xOffset: Int): Unit = {
+      ctx.save()
+
+      ctx.translate(ghost.x * pixelSize + pixelCentre + xOffset, ghost.y * pixelSize + pixelCentre - 10)
+      ctx.scale(1.0, 1.5)
+
       ctx.beginPath()
-      ctx.fillStyle = colour
-      ctx.arc(ghost.x * pixelWidth + 25, ghost.y * pixelHeight + 25, 20, 0.0, Math.PI * 2.0)
+      ctx.arc(0.0,
+        0.0,
+        4,
+        0.0,
+        Math.PI * 2.0)
+      ctx.fillStyle = "white"
       ctx.fill()
       ctx.closePath()
+
+      ctx.beginPath()
+      ctx.arc(0.0,
+        0.0,
+        2,
+        0.0,
+        Math.PI * 2.0)
+      ctx.fillStyle = "black"
+      ctx.fill()
+      ctx.closePath()
+
+      ctx.restore()
+    }
+
+    def drawGhost(ghost: Location, colour: String): Unit = {
+      // body
+      ctx.beginPath()
+      ctx.fillStyle = colour
+      ctx.arc(ghost.x * pixelSize + pixelCentre,
+              ghost.y * pixelSize + pixelCentre,
+              20,
+              0.0,
+              Math.PI * 2.0)
+      ctx.fill()
+      ctx.closePath()
+
+      // left eye
+      drawEye(ghost, -10)
+
+      // right eye
+      drawEye(ghost, 10)
     }
 
     ctx.fillStyle = "black"
@@ -95,17 +135,18 @@ object PacmanUI {
     // draw walls
     for (wall <- walls) {
       ctx.fillStyle = "blue"
-      ctx.fillRect(wall.x * pixelWidth + 10,
-                   wall.y * pixelHeight + 10,
-                   pixelWidth - 20,
-                   pixelHeight - 20)
+      ctx.fillRect(wall.x * pixelSize + 10, wall.y * pixelSize + 10, pixelSize - 20, pixelSize - 20)
     }
 
     // draw food
     for (food <- state.food) {
       ctx.beginPath()
       ctx.fillStyle = "yellow"
-      ctx.arc(food.x * pixelWidth + 25, food.y * pixelHeight + 25, 5, 0.0, Math.PI * 2.0)
+      ctx.arc(food.x * pixelSize + pixelCentre,
+              food.y * pixelSize + pixelCentre,
+              5,
+              0.0,
+              Math.PI * 2.0)
       ctx.fill()
       ctx.closePath()
     }
@@ -114,7 +155,11 @@ object PacmanUI {
     for (pill <- state.pills) {
       ctx.beginPath()
       ctx.fillStyle = "yellow"
-      ctx.arc(pill.x * pixelWidth + 25, pill.y * pixelHeight + 25, 15, 0.0, Math.PI * 2.0)
+      ctx.arc(pill.x * pixelSize + pixelCentre,
+              pill.y * pixelSize + pixelCentre,
+              15,
+              0.0,
+              Math.PI * 2.0)
       ctx.fill()
       ctx.closePath()
     }
@@ -138,12 +183,12 @@ object PacmanUI {
     }
     ctx.beginPath()
     ctx.fillStyle = "yellow"
-    ctx.arc(state.pacman.x * pixelWidth + 25,
-            state.pacman.y * pixelHeight + 25,
+    ctx.arc(state.pacman.x * pixelSize + pixelCentre,
+            state.pacman.y * pixelSize + pixelCentre,
             20.0,
             startAngle,
             endAngle)
-    ctx.lineTo(state.pacman.x * pixelWidth + 25, state.pacman.y * pixelHeight + 25)
+    ctx.lineTo(state.pacman.x * pixelSize + pixelCentre, state.pacman.y * pixelSize + pixelCentre)
     ctx.closePath()
     ctx.fill()
   }
